cde731a15100c3045fd22a0e017b9ce4163c5552
sakilaapokharel <pokharelsakila@gmail.com>
2023-07-18 15:58:35 +0545

    using document.write

--- W3School/externalscript.js
+++ W3School/externalscript.js
@@ -1,3 +1,9 @@
 function ExternalFunction(){
     document.getElementById("demo").innerHTML="Text Changed by external JS file";
 }
+
+document.write(11+29);
+
+function Add(){
+    document.write(11+29);
+}
