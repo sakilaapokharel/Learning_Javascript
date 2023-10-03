ede62abe52cc2dec3206bb8efe115a8cbed028fe
sakilaapokharel <pokharelsakila@gmail.com>
2023-10-03 15:17:46 +0545

    service section

--- Interior Design Website/script.js
+++ Interior Design Website/script.js
@@ -1,13 +1,26 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
+var mainImg = document.getElementById("mainimg");
+          var text1 = document.getElementById("text1");
+          var text2 = document.getElementById("text2");
+          var text3 = document.getElementById("text3");
+
+          text1.addEventListener("click",()=>{
+            text1.style.color="#053634"
+            text3.style.color="#84b8a7"
+            text2.style.color="#84b8a7"
+            mainImg.src = "img/service1.jpg"
+          })
+
+          text2.addEventListener("click",()=>{
+            text2.style.color="#053634"
+            text3.style.color="#84b8a7"
+            text1.style.color="#84b8a7"
+            mainImg.src = "img/service2.jpg"
+          })
+
+          text3.addEventListener("click",()=>{
+            text3.style.color="#053634"
+            text2.style.color="#84b8a7"
+            text1.style.color="#84b8a7"
+            mainImg.src = "img/service3.jpg"
+          })
+   
